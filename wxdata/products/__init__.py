7980158cfb354f72e9a94b5d474f48eda0908f22
Simon Pfreundschuh <simon.pfreundschuh@chalmers.se>
2020-06-08 12:47:51 +0200

    Export Modis-Aux product.

--- wxdata/products/__init__.py
+++ wxdata/products/__init__.py
@@ -2,7 +2,9 @@ import re
 from datetime import datetime
 from abc import ABCMeta, abstractmethod
 
-from wxdata.products.cloudsat import (CloudSat_1b_CPR, CloudSat_2b_GeoProf)
+from wxdata.products.cloudsat import (CloudSat_1b_CPR,
+                                      CloudSat_2b_GeoProf,
+                                      CloudSat_Modis_Aux)
 from wxdata.products.dardar import DardarCloud
 
 all_products = [CloudSat_1b_CPR,
